51f789a4608cf22fb817d22bad170b200efc862d
douglasmg7 <douglasmg7@gmail.com>
2020-03-12 10:11:09 -0300

    script to test zoom freight point to zoom api

--- bin/check-zoom-freight-www.sh
+++ bin/check-zoom-freight-www.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-curl -u test:1234 -X GET https://www.zunka.com.br/freightsrv/freights/zunka \
+curl -u test:1234 -X GET https://www.zunka.com.br/freightsrv/freights/zoom \
     -H "Content-Type: application/json" \
 	-d '{
 		"CEPDestiny": "31170210",
-		"productId":  "5e60eed63d13910785412eba"
+		"productId":  "5e63f1b72fb9bf051ecac4f4"
 	}'
 printf "\n"
 
